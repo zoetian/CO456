a7ad7f855323f28b659565ba8d71607ac792af5c
w3tian <w3tian@uwaterloo.ca>
2017-12-03 22:41:40 -0500

    [Script] parse.py added

--- Makefile
+++ Makefile
@@ -3,10 +3,15 @@ all:
 	javac *.java
 
 clean:
-	rm -rf *.class result.txt
+	rm -rf *.class result.txt analysis.txt
 
 fast:
-	rm -rf *.class result.txt
+	rm -rf *.class result.txt analysis.txt
 	javac *.java
 	wait
 	java Tournament full > result.txt
+	wait
+	python3 parse.py
+
+parse:
+	python3 parse.py
---
+++ parse.py
@@ -0,0 +1,52 @@
+resultFile = open("result.txt", "rt")
+
+with open("analysis.txt", "wt") as analysisFile:
+	keyword = "TeamWatermelon"
+	tmp = []
+	mine = []
+	oppo = []
+	cnt = 0
+	hit = False
+	teamNameList = []
+	scoreList = []
+
+	for line in resultFile:
+		tmp = line.split(' ')
+		if tmp[0] == "Average":
+			teamName = tmp[4][:-1]
+			score = tmp[5][:-1]
+			teamNameList.append(teamName)
+			scoreList.append(score)
+			cnt += 1
+			# print(teamName + " " + score)
+
+			# first case
+			if teamName == keyword and cnt %2 != 0:
+				mine.append(teamName + " " + score + " ")
+				hit = True
+			elif hit and cnt %2 == 0:
+				oppo.append(teamName + " " + score + " ")
+				hit = False
+				cnt = 0 # double check this one
+
+			# second case: when keywords occur later
+			# will require previous storage
+			elif teamName == keyword and cnt %2 == 0:
+				mine.append(teamName + " " + score + " ")
+				oppo.append(teamNameList[-2] + " " + scoreList[-2])
+
+fmt = '%-4s%-25s%s'
+
+print(fmt % ('', 'Mine', 'Oppo'))
+for i, (m, o) in enumerate(zip(mine, oppo)):
+    print(fmt % (i, m, o))
+
+# print("overall name: "+ str(teamNameList))
+# print()
+# print("overall score: "+ str(scoreList))
+# print()
+# print("mine: " + str(mine))
+# print()
+# print("oppo: " + str(oppo))
+analysisFile.close()
+resultFile.close()
